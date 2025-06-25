fba75db5996a984cd8b1ddd7e6a01a8b8cb56e8f
eulucaslim <lucaslimacnt14@gmail.com>
2025-06-24 23:03:48 -0400

    Add typescript and format components to tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './styles/global';
+import { Routes }  from './routes';
+
+export function App(){
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  )
+};
+
+
